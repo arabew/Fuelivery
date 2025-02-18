d7a7565fd9e0c70a1d84aaf643d53abeebac0e1a
arabew <arabew25@gmail.com>
2025-02-18 03:27:32 -0800

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fuelivery | Log In</title>
-   <link rel="stylesheet" href="login.css?v=1"> <!-- Link your CSS file -->
+   <link rel="stylesheet" href="login.css"> <!-- Link your CSS file -->
 </head>
 <body>
     <div class="login-container">
